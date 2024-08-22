ff423cc74e92a4c2470ec839211c09691112fa2f
Juankart65 <juane.cardonav@uqvirtual.edu.co>
2024-08-21 23:34:40 -0500

    Update

--- data.tsx
+++ data.tsx
@@ -4,7 +4,7 @@ export const socialNetworks = [
     {
         id: 1,
         logo: <Twitch size={30} strokeWidth={1} />,
-        src: "#!",
+        src: "https://kick.com/iamjuaness",
     },
     {
         id: 2,
@@ -14,7 +14,7 @@ export const socialNetworks = [
     {
         id: 3,
         logo: <Github size={30} strokeWidth={1} />,
-        src: "https://github.com/Juankart65",
+        src: "https://github.com/iamjuaness",
     },
     {
         id: 4,
@@ -65,31 +65,10 @@ export const itemsNavbar = [
 export const dataAboutPage = [
     {
         id: 1,
-        title: "Frontend Developer",
-        subtitle: "TechSolutions",
-        description: "Colabora con un equipo dinámico para desarrollar interfaces de usuario atractivas y funcionales que impulsen el éxito de nuestros clientes en el mundo digital.",
-        date: "Nov 2023 ",
-    },
-    {
-        id: 2,
-        title: "Creador de Experiencias Digitales",
-        subtitle: "PixelCrafters",
-        description: "Trabaja en proyectos emocionantes que desafían los límites de la creatividad y la tecnología. Únete a nosotros mientras creamos experiencias digitales cautivadoras que inspiran y cautivan a nuestros usuarios.",
-        date: "May 2021",
-    },
-    {
-        id: 3,
-        title: "Especialista en Desarrollo Frontend",
-        subtitle: "CodeForge Solutions",
-        description: "Como desarrollador frontend, tendrás la oportunidad de colaborar en proyectos diversos y desafiantes que te permitirán expandir tus habilidades y dejar tu huella en el mundo digital.",
-        date: "Ago 2019",
-    },
-    {
-        id: 4,
-        title: "Prácticas Grado",
-        subtitle: "WebWizards Inc.",
-        description: "Únete a nosotros mientras creamos sitios web y aplicaciones interactivas que sorprenden y deleitan a nuestros clientes. Si tienes pasión por el diseño y la programación, y disfrutas colaborar en un entorno creativo, ¡queremos conocerte!        ",
-        date: "Mar 2018",
+        title: "Practicas Universidad",
+        subtitle: "Unilocal",
+        description: "Plataforma web para la gestion de negocios!!!",
+        date: "May 2024",
     },
 ]
 
@@ -103,7 +82,7 @@ export const dataCounter = [
     },
     {
         id: 1,
-        endCounter: 10,
+        endCounter: 4,
         text: "Clientes satisfechos",
         lineRight: true,
         lineRightMobile: false,
@@ -127,8 +106,8 @@ export const dataCounter = [
 export const serviceData = [
     {
         icon: <Computer />,
-        title: "Desarrollo web",
-        description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
+        title: "Desarrollo backend",
+        description: "Desarrollo de aplicaciones usando Java, SpringBoot y otras tecnologias.",
     },
 
 ];
@@ -145,45 +124,10 @@ export const dataPortfolio = [
 
 export const dataTestimonials = [
     {
-        id: 1,
-        name: "George Snow",
-        description:
-            "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
-        imageUrl: "/profile1.png",
-    },
-    {
-        id: 2,
-        name: "Juan Pérez",
-        description:
-            "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
-        imageUrl: "/profile2.png",
-    },
-    {
-        id: 3,
-        name: "María García",
-        description:
-            "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
-        imageUrl: "/profile3.png",
-    },
-    {
-        id: 4,
-        name: "Laura Snow",
-        description:
-            "¡Qué descubrimiento tan fantástico! Los testimonios aquí son honestos y detallados. Me siento más seguro al tomar decisiones después de leer las experiencias compartidas por otros usuarios.",
-        imageUrl: "/profile4.png",
-    },
-    {
-        id: 5,
-        name: "Carlos Sánchez",
-        description:
-            "Una joya en la web. Los testimonios son fáciles de encontrar y están bien organizados. ¡Definitivamente mi destino número uno cuando necesito referencias confiables!",
-        imageUrl: "/profile5.png",
-    },
-    {
-        id: 6,
-        name: "Antonio Martínez",
-        description:
-            "¡Fantástico recurso para aquellos que buscan validación antes de tomar decisiones importantes! Los testimonios aquí son veraces y realmente útiles. ¡Gracias por simplificar mi proceso de toma de decisiones!",
-        imageUrl: "/profile6.png",
+        // id: 1,
+        // name: "George Snow",
+        // description:
+        //     "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
+        // imageUrl: "/profile1.png",
     },
 ];
